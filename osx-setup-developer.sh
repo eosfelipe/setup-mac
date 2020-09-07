6114d7b5bf3791652289ed79b1cc2e094aad33ab
Felipe Escobedo <escobedo.felipe@hotmail.com>
2020-09-07 16:16:38 -0500

    Add files via upload

---
+++ osx-setup-developer.sh
@@ -0,0 +1,23 @@
+xcode-select --install
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew update
+
+brew cask install iterm2
+
+#color foreground #02fbc7
+
+brew install cowsay
+
+brew install git
+
+brew cask install spectacle
+
+brew cask install alfred
+
+brew cask install firefox
+
+chsh -s $(which zsh)
+
+brew cask install visual-studio-code
